e03130bb54144ca291195a6fbeb1316793cb2a71
Soujanya <rejinthalsoujanya@gmail.com>
2025-11-04 11:10:51 +0530

    adding two files

---
+++ samplename.java
@@ -0,0 +1,8 @@
+public class samplename{
+    public static void main(String[] args){
+        //Declare a variable to hold the name
+        String name="Laasya sree";
+        //print the name to the console
+        System.out.println("Hello,my name is"+name);
+    }
+}
